b79c06b6dd985cccddad2853e8525b0cdea49b16
sushirach <rachelariellelow@gmail.com>
2024-11-03 17:22:58 +0800

    analytics component for profile view

---
+++ src/components/AnalyticsOverview.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="analytics-overview">
+      <h3>Analytics Overview</h3>
+      <div class="analytics-charts">
+        <div class="chart">
+          <h4>New Client Status</h4>
+          <img src="../assets/pie-chart.png" alt="New Client Status" />
+        </div>
+        <div class="chart">
+          <h4>Return Clients Status</h4>
+          <img src="../assets/bar-chart.png" alt="Return Clients Status" />
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      data: Object
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .analytics-overview {
+    padding: 2rem;
+  }
+  .analytics-charts {
+    display: flex;
+    gap: 2rem;
+  }
+  .chart {
+    flex: 1;
+  }
+  </style>
+  
