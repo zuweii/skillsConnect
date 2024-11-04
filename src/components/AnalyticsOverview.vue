9add70d6782fee93c61ece99cdeca1ead7a00597
sushirach <rachelariellelow@gmail.com>
2024-11-04 14:39:19 +0800

    analytics component updated

--- src/components/AnalyticsOverview.vue
+++ src/components/AnalyticsOverview.vue
@@ -1,37 +1,34 @@
 <template>
-    <section class="analytics-overview">
-      <h3>Analytics Overview</h3>
-      <div class="analytics-charts">
-        <div class="chart">
-          <h4>New Client Status</h4>
-          <img src="../assets/pie-chart.png" alt="New Client Status" />
-        </div>
-        <div class="chart">
-          <h4>Return Clients Status</h4>
-          <img src="../assets/bar-chart.png" alt="Return Clients Status" />
-        </div>
+  <section class="analytics-overview">
+    <h3>Analytics Overview</h3>
+    <div class="analytics-charts">
+      <div class="chart">
+        <img src="../assets/pie-chart.png" alt="New Client Status" />
       </div>
-    </section>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      data: Object
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .analytics-overview {
-    padding: 2rem;
+      <div class="chart">
+        <img src="../assets/bar-chart.png" alt="Return Clients Status" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object
   }
-  .analytics-charts {
-    display: flex;
-    gap: 2rem;
-  }
-  .chart {
-    flex: 1;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.analytics-overview {
+  padding: 2rem;
+}
+.analytics-charts {
+  display: flex;
+  gap: 2rem;
+}
+.chart {
+  flex: 1;
+}
+</style>
