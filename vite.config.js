2917dd54fbd806dcf40ca45ad60b0db1fdc07c54
zuweii <zuwei.tan.2023@scis.smu.edu.sg>
2024-11-10 00:05:03 +0800

    for deployment

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,30 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src')
     }
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   server: {
-    historyApiFallback: true
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
